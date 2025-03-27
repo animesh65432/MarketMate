619572e2fcb460a3670bb79838fd75ac342bf027
animesh65432 <kiranduttta234@gmail.com>
2025-03-27 21:26:30 +0530

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-dvh">
+      <div>name</div>
+    </div>
+  )
+}
+
+export default App
